0901d69edc80aee1ad1afe4fc63ddc9628736852
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-28 19:07:59 -0700

    Pass test 1 new prototype

--- src/journal.js
+++ src/journal.js
@@ -1,6 +1,11 @@
-function Entry(){
+function Entry(title, body){
   this.title = title;  
   this.body = body;
 }
 
-let journalEntry = new Entry(title, body);
+let journalEntry = new Entry("title", "body");
+
+Entry.prototype.wordCount = function(body) {
+  this.body = body;
+  return this.body;
+};
