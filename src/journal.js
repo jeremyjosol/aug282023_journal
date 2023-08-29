2b80fdca031c56981eb365232e4d80ab96230e4a
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-28 19:49:44 -0700

    Pass test 2 new prototype 2

--- src/journal.js
+++ src/journal.js
@@ -3,7 +3,7 @@ function Entry(title, body){
   this.body = body;
 }
 
-let journalEntry = new Entry("title", "This is the body this is the body");
+let journalEntry = new Entry("title", "This is a sentence in the body");
 
 Entry.prototype.wordCount = function(body) {
   const bodyCount = this.body.split(" ");
@@ -14,11 +14,14 @@ Entry.prototype.vowelCount = function(body) {
   const bodyCounter = this.body;
   const vowels = "aeiouAEIOU";
   let vowelCount = 0;
+  let consonantCount = 0;
 
   for (let i = 0; i < bodyCounter.length; i++) {
     if (vowels.includes(bodyCounter[i])){
       vowelCount++;
-    }  
-  }
-  return vowelCount;
-}
+    }  else {
+      consonantCount++;
+    }}
+    return "Vowels: " + vowelCount + " & " + "consonants: " + consonantCount;
+  };
+ 
